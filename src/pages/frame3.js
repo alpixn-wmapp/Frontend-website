d2e12761f27ec05f06d9cd6b07a5d5a2341c8726
Surya Pratap <sps64044@gmail.com>
2025-06-12 21:10:19 +0530

    third-c

--- src/pages/frame3.js
+++ src/pages/frame3.js
@@ -3,7 +3,7 @@ import React from 'react'
 const  Frame3=() =>{
   return (
     <div>
-        <h1 className='text-7xl text-black'>frame3</h1>
+        <h1 className='text-5xl text-black'>frame3</h1>
       
     </div>
   )
