740522edbc6645c902e86b635ea6ed16a18600c1
Surya Pratap <sps64044@gmail.com>
2025-06-12 22:30:48 +0530

    start

--- src/pages/frame3.js
+++ src/pages/frame3.js
@@ -1,27 +1,12 @@
 import React from 'react'
 
-<<<<<<< HEAD
-const  Frame3=() =>{
-  return (
-    <div>
-        <h1 className='text-5xl text-black'>frame3</h1>
-=======
 const frame3 = () => {
   return (
     <div>
-<<<<<<< HEAD
-        <h1 className='text-blue text-5xl'>THis is third frame3</h1>
->>>>>>> 131f937 (third-page)
-=======
-        <h1 className='text-blue text-5xl'>THi is third frame3</h1>
->>>>>>> 50c29cb (first)
+      <h1>hello</h1>
       
     </div>
   )
 }
 
-<<<<<<< HEAD
-export default Frame3
-=======
 export default frame3
->>>>>>> 131f937 (third-page)
