6f9fbe53f412b2beb5655055c8be983fa11c00a0
Surya Pratap <sps64044@gmail.com>
2025-06-12 22:19:03 +0530

    Resolved merge conflicts

--- src/pages/frame6.js
+++ src/pages/frame6.js
@@ -1,12 +1,25 @@
 import React from 'react'
 
+<<<<<<< HEAD
 const  Frame6=() =>{
   return (
     <div>
         <h1 className='text-8xl text-black'>frame6</h1>
+=======
+const frame6 = () => {
+  return (
+    <div>
+    <h3>
+        hi
+    </h3>
+>>>>>>> f0d2897 (Resolved merge conflicts)
       
     </div>
   )
 }
 
+<<<<<<< HEAD
 export default Frame6
+=======
+export default frame6
+>>>>>>> f0d2897 (Resolved merge conflicts)
