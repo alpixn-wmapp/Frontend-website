f0b3048cc7ab0b3d34bcd1bd93c3223789af0e37
Surya Pratap <sps64044@gmail.com>
2025-06-12 20:45:33 +0530

    Added  3 and 4

---
+++ Downloads/Frontend-website-main/Frontend-website/src/pages/frame3.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const  Frame3=() =>{
+  return (
+    <div>
+        <h1 className='text-8xl text-black'>frame3</h1>
+      
+    </div>
+  )
+}
+
+export default Frame3
