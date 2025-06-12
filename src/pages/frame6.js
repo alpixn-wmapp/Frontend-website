46abe5c40b5752d64ea8b09d088d03cb980eedfb
Surya Pratap <sps64044@gmail.com>
2025-06-12 20:58:20 +0530

    Added  3 and 4

---
+++ src/pages/frame6.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const  Frame6=() =>{
+  return (
+    <div>
+        <h1 className='text-8xl text-black'>frame6</h1>
+      
+    </div>
+  )
+}
+
+export default Frame6
