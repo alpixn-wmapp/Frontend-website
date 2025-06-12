16c22e8fac5dd0bc83519973aa04560eac09ed36
Surya Pratap <sps64044@gmail.com>
2025-06-12 22:24:43 +0530

    Resolved merge/rebase and updated changes

--- src/pages/frame3.js
+++ src/pages/frame3.js
@@ -1,12 +1,23 @@
 import React from 'react'
 
+<<<<<<< HEAD
 const  Frame3=() =>{
   return (
     <div>
         <h1 className='text-5xl text-black'>frame3</h1>
+=======
+const frame3 = () => {
+  return (
+    <div>
+        <h1 className='text-blue text-5xl'>THis is third frame3</h1>
+>>>>>>> 131f937 (third-page)
       
     </div>
   )
 }
 
+<<<<<<< HEAD
 export default Frame3
+=======
+export default frame3
+>>>>>>> 131f937 (third-page)
