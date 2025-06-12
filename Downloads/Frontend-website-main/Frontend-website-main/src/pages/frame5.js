7d5435b9a3714949391055fe37801e95d12e5159
Surya Pratap <sps64044@gmail.com>
2025-06-12 20:33:13 +0530

    Start-work

---
+++ Downloads/Frontend-website-main/Frontend-website-main/src/pages/frame5.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const  Frame5=() =>{
+  return (
+    <div>
+        <h1 className='text-5xl text-black'>frame5</h1>
+      
+    </div>
+  )
+}
+
+export default Frame5
---
+++ Downloads/Frontend-website-main/Frontend-website/src/pages/frame1.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function frame1() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default frame1
